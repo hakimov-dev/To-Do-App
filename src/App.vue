bf5601bd5de64e204435b5209324eadb062fb7b5
Muhammadamin <webhacker0100@gmail.com>
2022-08-10 15:07:55 +0500

    Update all

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="container pt-5" id="app">
+    <div class="card">
+      <h1>To do app</h1>
+      <div class="form-control">
+        <input
+          type="text"
+        />
+      </div>
+      <button class="btn primary">Add</button>
+      <h3 class="left">
+        Things to do: 10 ta
+      </h3>
+      <hr />
+      <ul class="list">
+        <h3 class="list-item" v-for="(note, idx) in 3">
+          {{ idx + 1 }} - Hello world
+          <button class="btn danger">
+            Delete
+          </button>
+        </h3>
+      </ul>
+      <!-- <h2 v-if="notes.length === 0">
+        Hozircha eslatmalar yo'q. Birinchisini qo'shing !
+      </h2> -->
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "App"
 };
 </script>
 
