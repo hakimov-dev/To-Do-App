58834771318b7f557c3bc1924cd10301ba58bdea
Muhammadamin <webhacker0100@gmail.com>
2022-08-10 15:01:04 +0500

    Import style css and disable eslint

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import '@/assets/style.css'
 
 createApp(App).use(store).mount("#app");
