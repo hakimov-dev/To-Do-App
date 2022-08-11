db27986ca28dd94333af9438d00c61f4beb63da4
Muhammadamin <webhacker0100@gmail.com>
2022-08-11 20:46:26 +0500

    Added transitions

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,23 @@
       </div>
       <button class="btn primary" @click="$store.commit('addNotes', $store.state.inputValue)">Add</button>
       <div class="left">
-        <button class="btn danger" v-if="$store.state.notes.length > 4"  @click="$store.commit('deletaAllNotes', $store.state.notes)">Delete all</button>
+        <transition>
+         <button class="btn danger" v-if="$store.state.notes.length > 4"  @click="$store.commit('deletaAllNotes', $store.state.notes)">Delete all</button>
+        </transition>
       </div>
       <hr />
+      <transition>
        <ul class="list" v-if="$store.state.notes.length !== 0">
         <h3 :class="note.isDone ? 'done' : ''" class="list-item" v-for="(note, idx) in $store.state.notes">
           <span class="todo-text">
           {{ idx + 1 }} - {{ note.note }}
           </span>
           <div>
-           <button v-if="!note.isDone" class="btn done-btn" @click="$store.commit('doneNote', idx)">
-             Done 
-           </button>
+           <transition>
+            <button v-if="!note.isDone" class="btn done-btn" @click="$store.commit('doneNote', idx)">
+              Done 
+            </button>
+           </transition>
            <button class="btn danger" @click="$store.commit('deleteNote', idx)">
              Delete
            </button>
@@ -33,6 +38,7 @@
       <h2 v-else>
         At the moment notes none. Add first!
       </h2>
+      </transition>
     </div>
   </div>
 </template>
